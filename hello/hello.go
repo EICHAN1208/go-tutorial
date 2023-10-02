bb9a505b63d5e4f93092aa1706ee95fe2977d830
EICHAN1208 <>
2023-10-02 20:02:44 +0900

    別モジュールの関数を呼び出す

--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
+	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Printf("Hello, World!\n")
 	fmt.Println(quote.Go())
+
+	message := greetings.Hello("Gladys")
+	fmt.Printf(message)
 }
