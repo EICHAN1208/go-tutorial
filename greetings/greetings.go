99df74f09b5ecacf76a6bdafe930c71de174f99f
EICHAN1208 <>
2023-10-07 10:57:27 +0900

    ランダムな挨拶を返す

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,29 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you , %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// 現在の時刻を使って疑似乱数のシードを設定する
+	// これを行わないと、毎回同じシーケンスの疑似乱数が生成されるため、
+	// 本当のランダムな動きを得ることができない
+	rand.Seed(time.Now().UnixNano())
+
+	return formats[rand.Intn(len(formats))]
 }
