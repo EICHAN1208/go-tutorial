cce907bdc645e82cf632d828aa975e269ce2c431
EICHAN1208 <>
2023-10-15 21:19:24 +0900

    return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -11,17 +11,22 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Gladys")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(message)
+	// message, err := greetings.Hello("Gladys")
+	// fmt.Println(message)
 
 	// fmt.Printf("Hello, World!\n")
 	// fmt.Println(quote.Go())
 
 	// message := greetings.Hello("Gladys")
 	// fmt.Printf(message)
+
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
 }
